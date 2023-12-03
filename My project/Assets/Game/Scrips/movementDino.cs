67c5b03c8244e64b7f5ee375336d2c95cd2db4fd
Aline Marchiori <alineeuu@outlook.com>
2023-12-03 13:10:32 -0300

    adicionado script para movimentar dino

---
+++ My project/Assets/Game/Scrips/movementDino.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementDino : MonoBehaviour{
+    
+    public float velocidadeMovimento;
+    public float forcaPulo;
+
+    private Rigidbody2D rigbody2d;
+    public SpriteRenderer spriteRenderer;
+
+    public Transform detectorChaoDino;
+    public float raioDeteccao;
+    public LayerMask layerChao;
+
+    private bool pulando;
+    private bool estaNoChao;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movepersonagem();
+        Pular();
+    }
+
+
+    public bool EstaNoChao{
+        get {
+            return this.estaNoChao;
+        }
+    }
+
+
+    void Movepersonagem(){
+
+        float horizontal = Input.GetAxis("Horizontal");
+        Vector2 velocidade = this.rigbody2d.velocity;
+        velocidade.x = horizontal * this.velocidadeMovimento;
+        this.rigbody2d.velocity = velocidade;
+
+        if (velocidade.x > 0){
+            this.spriteRenderer.flipX = false;
+        } else if (velocidade.x < 0){
+            this.spriteRenderer.flipX = true;
+        }
+    }
+
+
+    private void OnDrawinGizmos(){
+        Gizmos.DrawWireSphere(this.detectorChaoDino.position, this.raioDeteccao);
+    }
+
+
+    void Pular(){
+
+        Collider2D collider = Physics2D.OverlapCircle(this.detectorChaoDino.position, this.raioDeteccao, this.layerChao);
+        
+        if (collider != null){
+            this.estaNoChao = true;
+            this.pulando = false;
+        } else {
+            this.estaNoChao = false;
+        }
+
+        if (this.estaNoChao){
+            if (Input.GetKeyDown(KeyCode.Space)){
+                if (!this.pulando){
+                    Vector2 forca = new Vector2(0, this.forcaPulo);
+                    this.rigbody2d.AddForce(forca, ForceMode2D.Impulse);
+                }
+            }
+        }
+
+    }
+}
